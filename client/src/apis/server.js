3bc696f6d61548dedf43c78554df4d17149ff7f3
YahavMizrahi <yahav283@gmail.com>
2020-09-11 09:39:29 +0300

    apis

---
+++ client/src/apis/server.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+// export default axios.create({
+//     baseURL: 'http://localhost:3005'
+// });
+
+const server = axios.create({
+  baseURL: "http://localhost:3005",
+});
+
+// process.env.REACT_APP_LOCALHOST_RESTFUL
+
+export { server };
